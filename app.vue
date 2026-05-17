d50f07ea5f0a931ed8c90273d1ed54005282e4f0
极客先生 <geekmister@jikexianshengdeMacBook-Pro.local>
2026-05-17 17:57:32 +0800

    fix(vue): import Sidebar as default export in app.vue

--- app.vue
+++ app.vue
@@ -1,11 +1,13 @@
 <template>
+  <Sidebar />
   <NuxtPage />
 </template>
 
 <script setup lang="ts">
+import Sidebar from '~/components/Sidebar.vue';
 useHead({
   bodyAttrs: {
-    class: "bg-gray-50 dark:bg-gray-900 text-gray-800 dark:text-gray-100",
+    class: 'bg-gray-50 dark:bg-gray-900 text-gray-800 dark:text-gray-100',
   },
 });
 </script>
